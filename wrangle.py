31f2a09262c52805386f9e9b1fb4d012ae5e9cf1
Desiree_McElroy <desireeashley90@gmail.com>
2021-09-16 16:19:23 -0500

    adding imputer and split functions

--- wrangle.py
+++ wrangle.py
@@ -8,11 +8,11 @@ import matplotlib.pyplot as plt
 
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import RobustScaler
+# from sklearn.preprocessing import RobustScaler
+
+
 
 # This wrangle file holds all of my acquisition and data preparation functions for my notebook.
-
-
 
 def get_info(df):
     '''
@@ -36,4 +36,89 @@ def get_info(df):
     print()
     return df.sample(3)
 
+
+def value_counts(df, column):
+    '''
+    This function takes in a dataframe and list of columns and prints value counts for each column.
+    '''
+    for col in column:
+        print(col)
+        print(df[col].value_counts())
+        print('-------------')
+
+
+def show_outliers(df, k, columns):
+    '''
+    calculates the lower and upper bound to locate outliers and displays them
+    note: recommended k be 1.5 and entered as integer
+    '''
+    for i in columns:
+        quartile1, quartile3 = np.percentile(df[i], [25,75])
+        IQR_value = quartile3 - quartile1
+        lower_bound = (quartile1 - (k * IQR_value))
+        upper_bound = (quartile3 + (k * IQR_value))
+        print(f'For {i} the lower bound is {lower_bound} and  upper bound is {upper_bound}')
+
+
+def remove_outliers(df, k, columns):
+    '''
+    calculates the lower and upper bound to locate outliers in variables and then removes them.
+    note: recommended k be 1.5 and entered as integer
+    '''
+    for i in columns:
+        quartile1, quartile3 = np.percentile(df[i], [25,75])
+        IQR_value = quartile3 - quartile1
+        lower_bound = (quartile1 - (k * IQR_value))
+        upper_bound = (quartile3 + (k * IQR_value))
+        print(f'For {i} the lower bound is {lower_bound} and  upper bound is {upper_bound}')
+    
+    
+        df = df[(df[i] <= upper_bound) & (df[i] >= lower_bound)]
+        print('-----------------')
+        print('Dataframe now has ', df.shape[0], 'rows and ', df.shape[1], 'columns')
+    return df
+
+
+### IMPUTER Function
+def impute(df, strategy_method, column_list):
+    ''' take in a df, strategy, and cloumn list
+        return df with listed columns imputed using input stratagy
+    '''
+        
+    imputer = SimpleImputer(strategy=strategy_method)  # build imputer
+
+    df[column_list] = imputer.fit_transform(df[column_list]) # fit/transform selected columns
+
+    return df
+
+
+def split_data(df):
+    '''
+    This function takes in a dataframe and splits it into train, test, and 
+    validate dataframes for my model
+    '''
+
+    train_validate, test = train_test_split(df, test_size=.2, 
+                                        random_state=123)
+    train, validate = train_test_split(train_validate, test_size=.3, 
+                                   random_state=123)
+
+    print('train--->', train.shape)
+    print('validate--->', validate.shape)
+    print('test--->', test.shape)
+    return train, validate, test
+
+
+
+
+def split_ytarget(train, validate, test, y_target):
+    '''
+    This function takes in split data and a y_target variable and creates X and y
+    dataframes for each. Note: enter y_target as a string string
+    '''
+    X_train, y_train = train.drop(columns=[y_target]), train[y_target]
+    X_validate, y_validate = validate.drop(columns=[y_target]), validate[y_target]
+    X_test, y_test = test.drop(columns=[y_target]), test[y_target]
+    
+    return X_train, y_train, X_validate, y_validate, X_test, y_test
 
